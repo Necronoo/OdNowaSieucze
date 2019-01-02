c745c78b8103d3ffd6188201716c2c0e8c0aa4c7
Michał Podejko <michal.podejko@gmail.com>
2019-01-02 19:50:06 +0100

    uda sie

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -3,6 +3,7 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("tescik dupeczki :>");
+        System.out.println("a teraz");
     }
 }
