486860a37b7e7a17a4101809ef8e3ef69eefedfa
Michał Podejko <michal.podejko@gmail.com>
2019-01-03 13:58:57 +0100

    nowosc

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -5,5 +5,25 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("tescik dupeczki :>");
         System.out.println("a teraz");
+        String Mamamija = "Hello";
+        String Tescik = "";
+        //3.6.5
+        if (!Mamamija.equals("")) {
+            System.out.println("Chuja!");
+        }
+        else {
+            System.out.println( "nie robie nic");
+        }
+
+        if (Tescik.length() == 0) {
+            System.out.println("Chuja! x2 " );
+        }
+        else {
+            System.out.println( "nie robie nic ");
+        }
+        char last = Mamamija.charAt(4);
+        System.out.println( last);
+
+
     }
 }
