b5afa9635039daed80c5bd1fbcb4a8266efaefae
Michał Podejko <michal.podejko@gmail.com>
2019-01-04 09:46:01 +0100

    Scanner PrintWriter etc

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -1,6 +1,10 @@
 package com.timbuchalka;
 
 import java.io.Console;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -14,19 +18,17 @@ public class Main {
         //3.6.5
         if (!Mamamija.equals("")) {
             System.out.println("Chuja!");
-        }
-        else {
-            System.out.println( "nie robie nic");
+        } else {
+            System.out.println("nie robie nic");
         }
 
         if (Tescik.length() == 0) {
-            System.out.println("Chuja! x2 " );
-        }
-        else {
-            System.out.println( "nie robie nic ");
+            System.out.println("Chuja! x2 ");
+        } else {
+            System.out.println("nie robie nic ");
         }
         char last = Mamamija.charAt(4);
-        System.out.println( last);
+        System.out.println(last);
 
 //3.7.1
 //        Scanner scanner = new Scanner(System.in);
@@ -34,11 +36,33 @@ public class Main {
 //        scanner.next();
 //        System.out.println( name);
 
-//Console cos = System.console();
-//String username = cos.readLine("Username : ");
-//char[] passowrd = cos.readPassword("Hasło :");
 
-System.out.printf("%tc", new Date()); // akutalna Data ;> 
+        System.out.printf("%tc", new Date()); // akutalna Data ;>
+        System.out.println();
+        File file = new File("/Users/michalpodejko/Desktop/new.txt");
+        try {
+            PrintWriter out = new PrintWriter(file);
+        out.println("magiczna pulapka "+ " siusiak " + last);
+        out.close();
+
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    }
+        try {
+            // Create a new Scanner object which will read the data
+            // from the file passed in. To check if there are more
+            // line to read from it we check by calling the
+            // scanner.hasNextLine() method. We then read line one
+            // by one till all lines is read.
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
+
 }
